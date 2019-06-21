9c6bd4dfa6471a6f4ff731e9990fccc624970f7a
xubowen <bowen.xu@bangdao-tech.com>
2019-06-21 14:59:23 +0800

    add nginx.config

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+import Tag from "@/model/Tag";
+
+export default [
+  new Tag(
+    "front",
+    "https://uigradients.com/static/images/favicon-32x32.png",
+    "颜色渐进",
+    "https://uigradients.com/#Portrait",
+    "前端"
+  ),
+  new Tag(
+    "backed",
+    "https://uigradients.com/static/images/favicon-32x32.png",
+    "颜色渐进",
+    "https://uigradients.com/#Portrait",
+    "后端"
+  )
+];
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+let Gzip = require("compression-webpack-plugin");
+const productionGzipExtensions = ["js", "css"];
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -7,12 +10,23 @@ module.exports = {
       enableInSFC: false
     }
   },
-
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.plugins.push(
+        new Gzip({
+          algorithm: "gzip",
+          test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      );
+    }
+  },
   publicPath: "/vuer",
   outputDir: undefined,
   assetsDir: undefined,
   runtimeCompiler: undefined,
-  productionSourceMap: undefined,
+  productionSourceMap: false,
   parallel: undefined,
   css: undefined,
   lintOnSave: undefined
